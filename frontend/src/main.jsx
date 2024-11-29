1caae57df26d078ca236cb3337d216472873bf6e
Henri Nieminen <henkkamoi02@gmail.com>
2024-11-29 13:45:03 +0200

    Change: remove an unused component import from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
-import App from './App.jsx'
 import router from './router.jsx'
 
 createRoot(document.getElementById('root')).render(
