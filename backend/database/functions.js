17f4278e28da019caf3e5ee4db1addac1b4c1bc0
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-03 16:02:00 +0200

    Change: removed all old database functions, starting from scratch

--- backend/database/functions.js
+++ backend/database/functions.js
@@ -1,37 +1,5 @@
 
-const findAll = () => {
-
-};
-
-
-const findById = () => {
-
-};
-
-
-const deleteById = () => {
-
-};
-
-
-const save = () => {
-
-};
-
-const put = () => {
-
-};
-
-const patch = () => {
-
-};
-
 
 module.exports = {
-    findAll,
-    findById,
-    deleteById,
-    save,
-    put,
-    patch,
+
 };
