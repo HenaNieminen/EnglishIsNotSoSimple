62cf1cdf2367a3cdee16c9b854cd84b0f2d0fdef
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-03 16:48:18 +0200

    Change: add comments to database.js about current structure

--- backend/database/database.js
+++ backend/database/database.js
@@ -7,7 +7,11 @@ const db = new sqlite3.Database(":memory:", (error) => {
         return;
     }
     db.serialize(() => {
-        db.run("CREATE TABLE wordpairs (id INTEGER PRIMARY KEY, wordpair TEXT)");
+        /*Decided to keep it simple for now. This will only support a single language and definition
+        and may not even work from the get go. Idea to expand this would be to add a separate translations
+        table which I will implement later*/
+        db.run("CREATE TABLE wordpairs (id INTEGER PRIMARY KEY AUTOINCREMENT, words TEXT);")
+            .run("INSERT INTO wordpairs (words) VALUES ('hello, terve');")
     });
 });
 
