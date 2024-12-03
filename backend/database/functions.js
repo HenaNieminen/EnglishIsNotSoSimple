a17f4023f95cde0809c856a5ca55badd19217f51
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-03 16:56:38 +0200

    Change: added comments to function.js

--- backend/database/functions.js
+++ backend/database/functions.js
@@ -1,16 +1,21 @@
 const db = require('./database');
+//Import the database
 
 const getWordpairs = () => {
     return new Promise((resolve, reject) => {
+        /*Query all the wordpairs from the table*/
         db.all('SELECT * FROM wordpairs', (err, rows) => {
             if (err) {
+                //Throw error with reject (Will need better error handling in the future
                 return reject(err);
             }
+            //Resolve returns all taken data
             resolve(rows);
         });
     });
 };
 
+//Export all the modules for the router.js
 module.exports = {
     getWordpairs
 };
