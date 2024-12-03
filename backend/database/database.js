7983f1d5d6fc0dc68d610793c6e3d95d9c9a0cf2
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-03 15:45:55 +0200

    Change: add a simple table yet to be developed

--- backend/database/database.js
+++ backend/database/database.js
@@ -2,5 +2,11 @@ const sqlite3 = require("sqlite3").verbose();
 
 //Initialize the database to ram
 const db = new sqlite3.Database(":memory:", (error) => {
-
+    if (error) {
+        console.error("Database creation failed:", error);
+        return;
+    }
+    db.serialize(() => {
+        db.run("CREATE TABLE wordpairs (id INTEGER PRIMARY KEY, wordpair TEXT)");
+    });
 });
