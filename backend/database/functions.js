f7ad5d96289963b473c0e6f0384e22f20ee3d2bd
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-03 16:21:34 +0200

    Change: made getWordpairs functional for testing and routing

--- backend/database/functions.js
+++ backend/database/functions.js
@@ -1,9 +1,14 @@
 const db = require('./database');
 
-
-
 const getWordpairs = () => {
-
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM wordpairs', (err, rows) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(rows);
+        });
+    });
 };
 
 module.exports = {
