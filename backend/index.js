c954dd73c75615c7f42311470659f19b676b78d9
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-03 13:58:01 +0200

    Change: fix router constant

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const Router = require("./routes/router.js");
+const router = require("./routes/router.js");
 const port = process.env.PORT || 3000;
 const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/", Router);
+app.use("/api/", router);
 
 app.listen(port, () => {
     console.log(`Activating RESTful at: ${port}`);
