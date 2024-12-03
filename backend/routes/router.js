34e70e16076bf05075123c3fa96d28ab2948cbb3
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-03 18:21:26 +0200

    Change: implemented a simple get for router.

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,7 +3,14 @@ const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
-
+router.get("/wordpairs", async (req, res) => {
+    try {
+        const data = await sqlite.getWordpairs();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error fetchig from database", error);
+    }
+});
 
 
 
