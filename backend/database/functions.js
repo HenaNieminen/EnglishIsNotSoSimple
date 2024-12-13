e54197f8efe73fd690a29033d542fa73404bf12c
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-14 00:34:32 +0200

    Add: getWordsByID to query certain words

--- backend/database/functions.js
+++ backend/database/functions.js
@@ -24,8 +24,23 @@ const getAllTranslations = () => {
     });
 };
 
+const getWordsById = (id) => {
+    return new Promise((resolve, reject) => {
+        db.get('SELECT * FROM words WHERE id = ?', [id], (err, row) => {
+            if (err) {
+                return reject({ status: 500, message: err.message });
+            }
+            if (!row) {
+                return reject({ status: 404, message: 'Word not found' });
+            }
+            resolve(row);
+        });
+    });
+};
+
 //Export all the modules for the router.js
 module.exports = {
     getAllWords,
     getAllTranslations,
+    getWordsById,
 };
