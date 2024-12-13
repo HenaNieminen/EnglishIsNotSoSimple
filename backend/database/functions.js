17f3f2bc1c348bc848e928a448b827abe8bbd048
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-14 00:43:03 +0200

    Add: getTranslationsByID function to get translations by requested id

--- backend/database/functions.js
+++ backend/database/functions.js
@@ -38,6 +38,21 @@ const getWordsById = (id) => {
     });
 };
 
+const getTranslationsById = (id) => {
+    return new Promise((resolve, reject) => {
+        db.get('SELECT * FROM words WHERE id = ?', [id], (err, row) => {
+            if (err) {
+                return reject({ status: 500, message: err.message });
+            }
+            if (!row) {
+                return reject({ status: 404, message: 'Tanslations not found' });
+            }
+            resolve(row);
+        });
+    });
+};
+
+
 //Export all the modules for the router.js
 module.exports = {
     getAllWords,
