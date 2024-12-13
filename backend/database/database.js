0ded06b9eade3b5c04796deeef58d15b3f9c0ee0
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-14 00:02:04 +0200

    Add: createWordsTable and createTranslationsTable to split the database creation

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,31 +1,38 @@
 const sqlite3 = require("sqlite3").verbose();
 
-//Initialize the database to ram
+//Create words table
+const createWordsTable = (db) => {
+    db.run("CREATE TABLE words (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT);");
+};
+
+//Create translations table
+const createTranslationsTable = (db) => {
+    db.run(`CREATE TABLE translations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        word_id INTEGER NOT NULL,
+        translations text,
+        FOREIGN KEY (word_id) REFERENCES words (id)
+    );`);
+    /* This may become confusing at first, but we'll see how it pans out when I get to use it in the
+    frontend. Will need some handling. Case sensitivity, I will handle in the frontend and will most likely
+    force all posted words to be in lowercase */
+};
+
+// Initialize the database to RAM
 const db = new sqlite3.Database(":memory:", (error) => {
     if (error) {
         console.error("Database creation failed:", error);
         return;
     }
     db.serialize(() => {
-        db.run("CREATE TABLE words (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT);")
-            .run(`CREATE TABLE translations (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                word_id INTEGER NOT NULL,
-                translations text,
-                FOREIGN KEY (word_id) REFERENCES words (id)
-            );`)
-            /*Translations stores all the IDs of the words that match comma separated.
-            this will need some handling or a better idea*/
-            .run("INSERT INTO words (word) VALUES ('hello'), ('terve'), ('hi')")
+        createWordsTable(db);
+        createTranslationsTable(db);
+
+        db.run("INSERT INTO words (word) VALUES ('hello'), ('terve'), ('hi')")
             .run("INSERT INTO translations (word_id, translations) VALUES (1, '2,3'), (2, '1'), (3, '1');");
-
-        /*This may become confusing at first, but well see how it pans out when I get to use it in the
-        frontend. Will need some handling. Case sensitivity, I will handle in the frontend and will most likely
-        force all posted words to be in lowercase*/
-
-        /*Eventually, I will read off all the initial data from a file. Hardcoding should do
+        /* Eventually, I will read off all the initial data from a file. Hardcoding should do
         for now. For saving user generated words and translations, I will look into localstorage
-        or somehow making this persistant other means */
+        or somehow making this persistent other means */
     });
 });
 
