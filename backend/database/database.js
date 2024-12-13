7919e01762cd8593963d445baf5226fc76d74c9d
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-13 15:13:04 +0200

    Change: add a new place holder word and example of translations table data

--- backend/database/database.js
+++ backend/database/database.js
@@ -16,7 +16,13 @@ const db = new sqlite3.Database(":memory:", (error) => {
             );`)
             /*Translations stores all the IDs of the words that match comma separated.
             this will need some handling or a better idea*/
-            .run("INSERT INTO words (word) VALUES ('hello'), ('terve');");
+            .run("INSERT INTO words (word) VALUES ('hello'), ('terve'), ('hi')")
+            .run("INSERT INTO translations (word_id, translations) VALUES (1, '2,3'), (2, '1') (3, '1');");
+
+        /*This may become confusing at first, but well see how it pans out when I get to use it in the
+        frontend. Will need some handling. Case sensitivity, I will handle in the frontend and will most likely
+        force all posted words to be in lowercase*/
+
         /*Eventually, I will read off all the initial data from a file. Hardcoding should do
         for now. For saving user generated words and translations, I will look into localstorage
         or somehow making this persistant other means */
