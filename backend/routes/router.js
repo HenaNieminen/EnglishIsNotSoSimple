a28bb40025cb08c9f8ce4d9ec68ec8ea3bbbb45c
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-13 15:12:33 +0200

    Add: route to translations

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -15,6 +15,15 @@ router.get("/words", async (req, res) => {
     }
 });
 
+router.get("/translations", async (req, res) => {
+    try {
+        const data = await sqlite.getAllTranslations();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error fetchig from database", error);
+    }
+});
+
 
 
 module.exports = router;
