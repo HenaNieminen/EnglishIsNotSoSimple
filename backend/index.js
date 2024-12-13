e8f23439ba75b98d9d797f3e3f5766a3c4775c56
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-13 16:09:23 +0200

    Add: missing home route get. Nothing descriptive yet

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,15 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/api/", router);
+
+app.get("/", async (req, res) => {
+    try {
+        res.status(200).send("Welcome to EnglishIsNotSoSimple API");
+    } catch (error) {
+        res.status(500).send("Something went terribly wrong");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Activating RESTful at: ${port}`);
