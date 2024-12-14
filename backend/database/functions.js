e5ab2be289c33a1e3688850f37b502750a3f78e4
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-14 14:48:18 +0200

    Change: More error handling, code comments

--- backend/database/functions.js
+++ backend/database/functions.js
@@ -7,6 +7,10 @@ const getAllWords = () => {
             if (err) {
                 return reject({ status: 500, message: err.message });
             }
+            //If no words found
+            if (rows.length === 0) {
+                return reject({ status: 404, message: 'No words found' });
+            }
             //Resolve returns all taken data
             resolve(rows);
         });
@@ -19,6 +23,11 @@ const getAllTranslations = () => {
             if (err) {
                 return reject({ status: 500, message: err.message });
             }
+            //If no translations found
+            if (rows.length === 0) {
+                return reject({ status: 404, message: 'No translations found' });
+            }
+            //Resolve returns all taken data
             resolve(rows);
         });
     });
@@ -27,12 +36,15 @@ const getAllTranslations = () => {
 const getWordsById = (id) => {
     return new Promise((resolve, reject) => {
         db.get('SELECT * FROM words WHERE id = ?', [id], (err, row) => {
+            //If something goes wrong in the server
             if (err) {
                 return reject({ status: 500, message: err.message });
             }
+            //If not found
             if (!row) {
                 return reject({ status: 404, message: 'Word not found' });
             }
+            //Resolve and give the specific row from database
             resolve(row);
         });
     });
@@ -41,12 +53,15 @@ const getWordsById = (id) => {
 const getTranslationsById = (id) => {
     return new Promise((resolve, reject) => {
         db.get('SELECT * FROM words WHERE id = ?', [id], (err, row) => {
+            //If something goes wrong in the server
             if (err) {
                 return reject({ status: 500, message: err.message });
             }
+            //If not found
             if (!row) {
                 return reject({ status: 404, message: 'Tanslations not found' });
             }
+            //Resolve and give the specific row from database
             resolve(row);
         });
     });
@@ -54,16 +69,21 @@ const getTranslationsById = (id) => {
 
 const postWords = (word) => {
     return new Promise((resolve, reject) => {
+        //Will refuse if the word is empty. Will also be handled in frontend for redundancy
         if (word.length < 1) {
             return reject({ status: 400, message: 'Word cannot be empty' });
         }
         db.run('INSERT INTO words (word) VALUES (?)', [word], function (err) {
             if (err) {
+                /*Handling for unique constraint. Since there is only one constraint,
+                you can just use generic SQLITE_CONSTRAIT and don't need to check
+                if it contains error for uniqueness*/
                 if (err.code === 'SQLITE_CONSTRAINT') {
                     return reject({ status: 409, message: 'Word already exists' });
                 }
                 return reject({ status: 500, message: err.message });
             }
+            //Resolve the word and the last id in the autoincrement
             resolve({ id: this.lastID, word: word });
         });
     });
