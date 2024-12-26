c24e471de21be0e50a12eb1c4b7fa40266083989
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-26 19:28:35 +0200

    Change: Edited database structure to use multiple rows for translations instead of comma separation

--- backend/database/database.js
+++ backend/database/database.js
@@ -9,18 +9,18 @@ const createWordsTable = (db) => {
 const createTranslationsTable = (db) => {
     db.run(`CREATE TABLE translations (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        word_id INTEGER NOT NULL UNIQUE,
-        translations text,
-        FOREIGN KEY (word_id) REFERENCES words (id)
+        word_id INTEGER NOT NULL,
+        translation_id INTEGER NOT NULL,
+        FOREIGN KEY (word_id) REFERENCES words (id),
+        FOREIGN KEY (translation_id REFRENCES words (id),
     );`);
-    /* This may become confusing at first, but we'll see how it pans out when I get to use it in the
-    frontend. Will need some handling. Case sensitivity, I will handle in the frontend and will most likely
-    force all posted words to be in lowercase */
+    /*Converted to use duplication. I realized that comma separating them
+    will cause headaches in the future. Luckily this is still easily correctable*/
 };
 
 const createPlaceHolderData = (db) => {
     db.run("INSERT INTO words (word) VALUES ('hello'), ('terve'), ('hi')")
-        .run("INSERT INTO translations (word_id, translations) VALUES (1, '2,3'), (2, '1'), (3, '1');");
+        .run("INSERT INTO translations (word_id, translation_id) VALUES (1, 2), (1, 3), (2, 1), (3, 1);");
     /* Eventually, I will read off all the initial data from a file. Hardcoding should do
     for now. For saving user generated words and translations, I will look into localstorage
     or somehow making this persistent other means */
