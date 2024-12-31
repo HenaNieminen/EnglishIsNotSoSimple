13d1e5a23be7b50e9d559963ed138b361086e91c
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-31 15:20:17 +0200

    Add: MainPage for inserting main menu view into and basic router implementation and mapping out

---
+++ frontend/src/pages/mainpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Helo helo</h1>
+        </div>
+    );
+};
+
+export default MainPage;
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
+import MainPage from './pages/mainpage.jsx';
 
-const router = createBrowserRouter();
-
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainPage />,
+    },
+    {
+        path: '/settings',
+    },
+]);
 
 export default router;
