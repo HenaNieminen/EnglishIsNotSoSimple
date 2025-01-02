de6cf9383a067dce33c5d5b0eb6fd1d811955214
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-02 19:58:15 +0200

    Change: deleted unused classnames

--- frontend/src/pages/errorpage.jsx
+++ frontend/src/pages/errorpage.jsx
@@ -4,10 +4,10 @@ import "../styles/main.css"
 export default function NotFoundPage() {
     //Returns an error element for the router
     return (
-        <div className="windowColumn">
+        <div>
             <h1>404: Not found</h1>
             <div>
-                <Link className="navButton" to="/">Go back to the main page</Link>
+                <Link to="/"> Go back to the main page</Link>
             </div>
         </div>
     );
