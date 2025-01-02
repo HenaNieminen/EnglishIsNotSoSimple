8d0d285ab0adad412d462340b6d517f8fd2c3b22
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-02 19:35:32 +0200

    Add: NotFoundPage as an error element for React router

---
+++ frontend/src/pages/errorpage.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import "../styles/main.css"
+
+export default function NotFoundPage() {
+    //Returns an error element for the router
+    return (
+        <div className="windowColumn">
+            <h1>404: Not found</h1>
+            <div>
+                <Link className="navButton" to="/">Go back to the main page</Link>
+            </div>
+        </div>
+    );
+};
