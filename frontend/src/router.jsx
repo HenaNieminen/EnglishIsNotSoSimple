bf633ae6381674e8c0c1b2532436e50bf4e55b2f
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-02 19:36:21 +0200

    Change: added NotFoundPage to router error elements

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from './pages/mainpage.jsx';
+import NotFoundPage from './pages/errorpage.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <MainPage />,
+        errorElement: <NotFoundPage/>,
     },
     {
         path: '/settings',
+        errorElement: <NotFoundPage />,
     },
 ]);
 
