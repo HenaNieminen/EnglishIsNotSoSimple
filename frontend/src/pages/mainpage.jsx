e7e694ee7dbff7939c7b5135d21dcfb0054dc942
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-04 23:00:03 +0200

    Change: revised MainPage and added settingspage

--- frontend/src/pages/mainpage.jsx
+++ frontend/src/pages/mainpage.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 
 const MainPage = () => {
     return (
-        <div>
-            <h1>Helo helo</h1>
-        </div>
+        <>
+        <h1>
+            Hola hola
+        </h1>
+        </>
     );
 };
 
---
+++ frontend/src/pages/settingsadmin.jsx
@@ -0,0 +1,11 @@
+import 'react-toastify/dist/ReactToastify.css';
+
+const SettingsPage = () => {
+    return (
+        <div>
+            <h1>Silent here</h1>
+        </div>
+    );
+};
+
+export default SettingsPage;
