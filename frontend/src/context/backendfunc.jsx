a895302d4d7fe7224b50d33da2c5a555e079ee46
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-04 02:50:38 +0200

    Add: fetchLanguages to fetch languages from the backend

--- frontend/src/context/backendfunc.jsx
+++ frontend/src/context/backendfunc.jsx
@@ -1,3 +1,17 @@
 import axios from 'axios';
 
-const URL = `/api/`;
+const fetchLanguages = async (setLanguages) => {
+    try {
+        //Get the env for the api url from the viteconfig
+        const URL = import.meta.env.VITE_API_URL;
+        const response = await axios.get(`${URL}languages`);
+        setLanguages(response.data);
+    } catch (error) {
+        console.error('Error fetching languages:', error);
+    }
+};
+
+
+export {
+    fetchLanguages
+};
