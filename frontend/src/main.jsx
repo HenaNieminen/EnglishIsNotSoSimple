b7c8a8ab2fb9af93034cc7863ec4dd5d9af1a2d6
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-04 16:38:03 +0200

    Change: added toastcontainers and stylesheets

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import router from './router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ToastContainer>
+        <RouterProvider router={router} />
+    </ToastContainer>
   </StrictMode>,
 )
