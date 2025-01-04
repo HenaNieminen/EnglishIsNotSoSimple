4bae1a3d46aa90737436565ae47154e0d5ffcb51
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-04 16:43:57 +0200

    Change: add Dataprovider to the very root to give all components context to pull data from

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { DataProvider } from './context/datacontext.jsx';
 import router from './router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ToastContainer>
-        <RouterProvider router={router} />
-    </ToastContainer>
+    <DataProvider>
+        <ToastContainer>
+            <RouterProvider router={router} />
+        </ToastContainer>
+    </DataProvider>
   </StrictMode>,
 )
