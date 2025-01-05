4893b3364367921283ad8ef8b3a578b3c42930b6
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-05 18:17:13 +0200

    Change: added empty try catch blocks to rest of functions later to be filled

--- frontend/src/context/backendfunc.jsx
+++ frontend/src/context/backendfunc.jsx
@@ -31,35 +31,67 @@ const fetchTrans = async () => {
 };
 
 const fetchLangById = async (id) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 const fetchWordsById = async (id) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 const fetchTransForWordId = async (id) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 const postWords = async (langId, word) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 const postTrans = async (id, transId) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 const deleteWords = async (id) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 const deleteTrans = async (wordId, transId) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 const patchWords = async (id, langId, word) => {
+    try {
 
+    } catch (error) {
+
+    }
 };
 
 export {
