7d2e805b0645b4150d8f2c756dd7aaaaaa40fd1e
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-06 01:25:11 +0200

    Add: empty quiz page and route

---
+++ frontend/src/pages/quizpage.jsx
@@ -0,0 +1,10 @@
+import "../styles/main.css"
+
+const QuizPage = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default QuizPage;
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainPage from './pages/mainpage.jsx';
 import NotFoundPage from './pages/errorpage.jsx';
 import SettingsPage from './pages/settingsadmin.jsx';
+import QuizPage from './pages/quizpage.jsx';
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,11 @@ const router = createBrowserRouter([
     {
         path: '/settings',
         element: <SettingsPage />,
+        errorElement: <NotFoundPage />,
+    },
+    {
+        path: '/quiz',
+        element: <QuizPage />,
         errorElement: <NotFoundPage />,
     },
 ]);
