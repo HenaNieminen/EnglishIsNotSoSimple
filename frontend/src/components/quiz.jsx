dae658622e25ff37a672870275566c2354b8c9bb
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-09 19:07:18 +0200

    Change: fixed generate questions to seek if words have translations first

--- frontend/src/components/quiz.jsx
+++ frontend/src/components/quiz.jsx
@@ -1,33 +1,39 @@
 import { useState, useEffect, useContext } from 'react';
-import { fetchTransForWordId } from '../context/backendfunc';
 import { DataContext } from '../context/datacontext';
+import { fetchTransForWordId } from '../context/backendfunc';
 import PropTypes from 'prop-types'
 import { TextField, Typography, Button, Box } from '@mui/material/';
 
 const Quiz = ({ language, length, active }) => {
-    const { words } = useContext(DataContext);
+    const { words, trans } = useContext(DataContext);
     const [questions, setQuestions] = useState([]);
     const [score, setScore] = useState(0);
     const [quizOver, setQuizOver] = useState(false);
     const [userAnswers, setUserAnswers] = useState([]);
 
+
     useEffect(() => {
         const generateQuestions = async () => {
             const langWords = words.filter(word => word.lang_id === language);
-            //Exit if words are not available
-            if (langWords.length === 0) {
-                console.warn('No words available for the selected language.');
+            const wordsWithTran = langWords.filter(word => trans.some(tran => tran.word_id === word.id));
+            if (wordsWithTran.length === 0) {
+                console.warn('No translations available for the selected words.');
+                active(false);
                 return;
-            };
+            }
+
             const tempArray = [];
             /*This new technique of using sets was suggested by co-pilot. Pretty good for
             weeding out anomalies and duplicates*/
             const usedWordIds = new Set();
-            //Maxlength will make questions as much as possible if user picks more than there are words
-            const maxLength = Math.min(length, langWords.length);
-            while (tempArray.length < maxLength) {
-                const randomIndex = Math.floor(Math.random() * langWords.length);
-                const randomWord = langWords[randomIndex];
+            //Will set the length as long as possible if there are not enough words than user requested
+            if (wordsWithTran.length < length) {
+                length = wordsWithTran.length;
+            }
+
+            while (tempArray.length < length) {
+                const randomIndex = Math.floor(Math.random() * length);
+                const randomWord = wordsWithTran[randomIndex];
                 //Skip if a randomly selected word is already there in the Set
                 if (usedWordIds.has(randomWord.id)) {
                     continue;
@@ -36,21 +42,21 @@ const Quiz = ({ language, length, active }) => {
                     const translations = await fetchTransForWordId(randomWord.id);
                     const transIds = translations.map(trans => trans.trans_id);
                     const answers = words.filter(word => transIds.includes(word.id));
-                    if (answers.length > 0) {
-                        tempArray.push({
-                            question: randomWord.word,
-                            answers: answers.map(answer => answer.word),
-                        });
-                        usedWordIds.add(randomWord.id);//Add used word to the Set
-                    };
+                    tempArray.push({
+                        question: randomWord.word,
+                        answers: answers.map(answer => answer.word),
+                    });
+                    usedWordIds.add(randomWord.id);//Add used word to the Set
                 } catch (error) {
                     console.error(`Error fetching translations for word ID ${randomWord.id}:`, error);
-                };
+                }
             }
             setQuestions(tempArray);
         };
-        generateQuestions();
-    }, [language, length, words]);
+        if (!quizOver) {
+            generateQuestions();
+        }
+    }, [language, length, words, trans, active, quizOver]);
 
     const handleSubmit = () => {
         //Handle the submitted answers
