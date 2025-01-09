a5f0b9a6d7052d7ef6720ebe9bd1dc9af035207c
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-09 23:00:38 +0200

    Change: imported elements from material ui

--- frontend/src/components/editor.jsx
+++ frontend/src/components/editor.jsx
@@ -6,6 +6,7 @@ import {
     patchWords,
     fetchTransForWordId,
 } from '../context/backendfunc';
+import { TextField, Typography, Button, Box } from '@mui/material/';
 //I am become import, the destroyer of coherency
 
 const Editor = () => {
