7e2df3b6682c73eab005e49fe4481a449e9c538c
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-09 22:59:22 +0200

    Change: imported functions to editor

--- frontend/src/components/editor.jsx
+++ frontend/src/components/editor.jsx
@@ -1,8 +1,16 @@
 import { useContext } from 'react';
 import { DataContext } from '../context/datacontext';
+import {
+    deleteWords,
+    deleteTrans,
+    patchWords,
+    fetchTransForWordId,
+} from '../context/backendfunc';
+//I am become import, the destroyer of coherency
 
 const Editor = () => {
     const { langs, words, trans, syncData } = useContext(DataContext);
+    const [ editedWord, setEditedWord ] = useState('');
 
     return (
         <>
