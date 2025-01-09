a1befe82e607921a0db6f5a282306035facb10ab
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-09 14:40:51 +0200

    Change: switched from spanish to english

--- frontend/src/pages/mainpage.jsx
+++ frontend/src/pages/mainpage.jsx
@@ -4,10 +4,10 @@ import "../styles/main.css"
 const MainPage = () => {
     return (
         <div className="mainContainer">
-            <h1>Hola hola</h1>
+            <h1>Welcome!</h1>
             <div className='navBar'>
                 <Link className="navButton" to="/settings">Admin/settings</Link>
-                <Link className="navButton" to= "/quiz">Start quiz</Link>
+                <Link className="navButton" to= "/quiz">To quiz</Link>
             </div>
         </div>
     );
