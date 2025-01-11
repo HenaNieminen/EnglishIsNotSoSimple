20871b2eedc3de8fb27b46f9185d6b6f1277b9c0
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-11 16:59:20 +0200

    Change: added imports to Adder

---
+++ frontend/src/components/adder.jsx
@@ -0,0 +1,9 @@
+import { useContext, useState } from 'react';
+import { DataContext } from '../context/datacontext';
+import { postWords } from '../context/backendfunc'
+import { TextField, Typography, Button, Box } from '@mui/material/';
+
+const Adder = () => {
+
+
+}
