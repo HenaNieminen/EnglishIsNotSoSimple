79d4c352354a1589b4cd5639d2b151902207f4fd
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-11 22:05:55 +0200

    Change: some basic structure to adding words

--- frontend/src/components/adder.jsx
+++ frontend/src/components/adder.jsx
@@ -4,6 +4,22 @@ import { postWords } from '../context/backendfunc'
 import { TextField, Typography, Button, Box } from '@mui/material/';
 
 const Adder = () => {
+    const { syncData } = useContext(DataContext);
 
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minWidth: 400,
+            maxWidth: 900,
+            maxHeight: 200,
+            margin: 'auto',
+            backgroundColor: '#525252',
+            padding: 5
+        }}>
 
+        </Box>
+    );
 }
+
+export default Adder;
