5d3ad12c8cf70ef188c9ffbe46cd01fc6c03130d
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-12 01:38:42 +0200

    Change: added more frameworks to adding word. Non-functional just yet

--- frontend/src/components/adder.jsx
+++ frontend/src/components/adder.jsx
@@ -4,7 +4,18 @@ import { postWords } from '../context/backendfunc'
 import { TextField, Typography, Button, Box } from '@mui/material/';
 
 const Adder = () => {
-    const { syncData } = useContext(DataContext);
+    const { langs, words, syncData } = useContext(DataContext);
+    const [ postedWord, setPostedWord ] = useState('');
+    const [ postedLang, setPostedLang ] = useState(0);
+
+    const sendWord = async (lang_id, word) => {
+        try {
+            const postedWord = { lang_id, word };
+            postWords(postedWord);
+        } catch (error) {
+            console.error('Error posting word', error);
+        };
+    };
 
     return (
         <Box sx={{
@@ -17,7 +28,21 @@ const Adder = () => {
             backgroundColor: '#525252',
             padding: 5
         }}>
-
+            <TextField
+                variant="outlined"
+                value={postedWord}
+                onChange={(e) => setPostedWord(e.target.value)}
+                sx={{ backgroundColor: "white", marginBottom: 2 }}
+            />
+            <Button
+                variant="contained"
+                onClick={async () => {
+                    sendWord(postedLang, postedWord);
+                }}
+                sx={{ marginTop: 3 }}
+            >
+                Save
+            </Button>
         </Box>
     );
 }
