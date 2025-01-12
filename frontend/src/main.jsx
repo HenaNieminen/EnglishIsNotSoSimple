0cb13c3005ed26a079ae23fc43b7f744e3b5d32a
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-13 00:55:58 +0200

    Change: adjusted toast container

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import { DataProvider } from './context/datacontext.jsx';
 import router from './router.jsx';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
+    <React.StrictMode>
         <DataProvider>
-            <ToastContainer />
+            <ToastContainer position="top-right" autoClose={3000} />
             <RouterProvider router={router} />
         </DataProvider>
-    </StrictMode>,
+    </React.StrictMode>,
 );
