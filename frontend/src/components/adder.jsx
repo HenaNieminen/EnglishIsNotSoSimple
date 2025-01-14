59853572004b829bf2a1c974a9fe51a1c918c64a
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-14 02:54:30 +0200

    Change: add menu is now hidden untill user presses a button

--- frontend/src/components/adder.jsx
+++ frontend/src/components/adder.jsx
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 
 const Adder = () => {
     const { langs, syncData } = useContext(DataContext);
+    const [ addMode, setAddMode ] = useState(false);
     const [ postedWord, setPostedWord ] = useState('');
     const [ postedLang, setPostedLang ] = useState('');
     const [ postedTrans, setPostedTrans ] = useState('');
@@ -26,54 +27,87 @@ const Adder = () => {
     };
 
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            minWidth: 400,
-            maxWidth: 900,
-            maxHeight: 200,
-            margin: 'auto',
-            backgroundColor: '#525252',
-            padding: 5
-        }}>
-            <Typography variant="h5" sx={{color: 'white'}}>Add a new word</Typography>
-            <FormControl sx={{ marginBottom: 2 }}>
-                <InputLabel id="language-select-label">Language</InputLabel>
-                <Select
-                    labelId="language-select-label"
-                    value={postedLang}
-                    size="small"
-                    onChange={(e) => setPostedLang(e.target.value)}
-                    label="Language"
-                    sx={{ backgroundColor: "white" }}
-                >
-                    {langs.map((lang) => (
-                        <MenuItem key={lang.id} value={lang.id}>
-                            {lang.language}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <TextField
-                variant="outlined"
-                size="small"
-                label="New word"
-                value={postedWord}
-                onChange={(e) => setPostedWord(e.target.value)}
-                sx={{ backgroundColor: "white", marginBottom: 2 }}
-            />
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginTop: 5,
+            }}
+        >
+        {/*Button to open the menu*/}
+        {!addMode && (
             <Button
                 variant="contained"
-                disabled={!postedLang || !postedWord}
-                onClick={async () => {
-                    await sendWord(postedLang, postedWord);
-                }}
-                sx={{ marginTop: 3 }}
+                onClick={() => setAddMode(true)}
+                sx={{ marginBottom: 3 }}
             >
-                Save
+                Add new word
             </Button>
+            )}
+        {addMode && (
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minWidth: 400,
+                    maxWidth: 900,
+                    maxHeight: 200,
+                    margin: 'auto',
+                    backgroundColor: '#525252',
+                    padding: 5,
+                    }}
+                >
+                <Typography variant="h5" sx={{ color: 'white', marginBottom: 1 }}> Add a new word</Typography>
+                    <FormControl sx={{ marginBottom: 2 }}>
+                        <InputLabel id="language-select-label">Language</InputLabel>
+                        <Select
+                            labelId="language-select-label"
+                            value={postedLang}
+                            size="small"
+                            onChange={(e) => setPostedLang(e.target.value)}
+                            label="Language"
+                            sx={{ backgroundColor: 'white' }}
+                        >
+                            {langs.map((lang) => (
+                                <MenuItem key={lang.id} value={lang.id}>
+                                    {lang.language}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <TextField
+                        variant="outlined"
+                        size="small"
+                        label="New word"
+                        value={postedWord}
+                        onChange={(e) => setPostedWord(e.target.value)}
+                        sx={{ backgroundColor: 'white', marginBottom: 2 }}
+                    />
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Button
+                            variant="contained"
+                            color="error"
+                            onClick={() => setAddMode(false)}
+                            sx={{ marginTop: 3 }}
+                        >
+                            Close
+                        </Button>
+                        <Button
+                            variant="contained"
+                            disabled={!postedLang || !postedWord}
+                            onClick={async () => {
+                                await sendWord(postedLang, postedWord);
+                            }}
+                            sx={{ marginTop: 3 }}
+                        >
+                            Save
+                        </Button>
+                    </Box>
+                </Box>
+            )}
         </Box>
     );
-}
+};
 
 export default Adder;
