68ad7d641604f4aa6b3415b5834144db79aa776e
Henri Nieminen <henkkamoi02@gmail.com>
2025-01-15 22:09:54 +0200

    Change: added comments to paths

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,24 +4,27 @@ import NotFoundPage from './pages/errorpage.jsx';
 import SettingsPage from './pages/settingsadmin.jsx';
 import QuizPage from './pages/quizpage.jsx';
 
+//React router configuration
 const router = createBrowserRouter([
+    //Path to main page
     {
         path: '/',
         element: <MainPage />,
         errorElement: <NotFoundPage />,
     },
+    //Path to settings
     {
         path: '/settings',
         element: <SettingsPage />,
         errorElement: <NotFoundPage />,
     },
+    //Path to quiz
     {
         path: '/quiz',
         element: <QuizPage />,
         errorElement: <NotFoundPage />,
     },
-    /*In case you went directly to the quiz page, this will
-    redirect you back to the main page*/
+    /*In case you went anywhere else, throw the notfoundpage*/
     {
         path: '*',
         element: <NotFoundPage />,
